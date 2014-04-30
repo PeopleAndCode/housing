838a8332be043b0001a5bc2de3195efb7fff71ea
Naomi <naomi@addictivemobility.com>
2014-04-30 10:13:58 -0400

    configure user_mailer welcome

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def welcome_email(user)
+  	@user = user
+  	@url = "http://repairs-system.herokuapp.com/users/sign_in"
+  	mail(to: @user.email, subject: 'Welcome to this awesome site!')
+  end
 end
