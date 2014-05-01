82e0c8133c26b1087d11146146bafed978f7d030
Naomi <naomi@addictivemobility.com>
2014-05-01 15:31:34 -0400

    trying to get the mailer working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         # Tell the UserMailer to send a welcome email after save
-        UserMailer.welcome_email(@user.deliver)
+        #UserMailer.welcome_email(@user.deliver)
         
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.js
